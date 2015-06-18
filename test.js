425a58a3f000bd46e792081e9072ccb0382a32f5
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-06-18 21:23:49 +0200

    Use `float-equal` in the tests
    Fix #2

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 'use strict';
 var test = require('ava');
 var requireUncached = require('require-uncached');
+var floatEqual = require('float-equal');
 
 Math.atanh = undefined;
 var atanh = require('./');
@@ -39,15 +40,15 @@ test('If x is 0, the result is 0', function (t) {
 });
 
 test('Returns an implementation-dependent approximation to the inverse hyperbolic tangent of x', function (t) {
-	t.assert(atanh(0.1).toFixed(5) === '0.10034');
-	t.assert(atanh(0.2).toFixed(5) === '0.20273');
-	t.assert(atanh(0.3).toFixed(5) === '0.30952');
-	t.assert(atanh(0.4).toFixed(5) === '0.42365');
-	t.assert(atanh(0.5).toFixed(5) === '0.54931');
-	t.assert(atanh(0.6).toFixed(5) === '0.69315');
-	t.assert(atanh(0.7).toFixed(5) === '0.86730');
-	t.assert(atanh(0.8).toFixed(5) === '1.09861');
-	t.assert(atanh(0.9).toFixed(5) === '1.47222');
+	t.assert(floatEqual(atanh(0.1), 0.10033534773107562));
+	t.assert(floatEqual(atanh(0.2), 0.2027325540540821));
+	t.assert(floatEqual(atanh(0.3), 0.3095196042031118));
+	t.assert(floatEqual(atanh(0.4), 0.42364893019360184));
+	t.assert(floatEqual(atanh(0.5), 0.5493061443340549));
+	t.assert(floatEqual(atanh(0.6), 0.6931471805599453));
+	t.assert(floatEqual(atanh(0.7), 0.8673005276940532));
+	t.assert(floatEqual(atanh(0.8), 1.0986122886681098));
+	t.assert(floatEqual(atanh(0.9), 1.4722194895832204));
 });
 
 test('Return 0 when passing null', function (t) {
